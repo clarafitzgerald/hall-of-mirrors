25da35fb55e4f3a76223240c5aadf4b9869d1aef
Clara Fitzgerald <clara.fitzgerald@Nology.local>
2019-11-08 12:00:36 +0000

    somewhat funcitoning

---
+++ src/components/Image/Image.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class  extends Component {
+  render() { 
+    return ( 
+      <img></img>
+      src={this.props.src}
+      alt="display"
+      style={transformation}
+      key={index}
+      rotation={randomNumber}
+     );
+  }
+}
+ 
+export default ;
