ab2224ed491785ba1abec43d1c03f32e084654f7
Clara Fitzgerald <clara.fitzgerald@Nology.local>
2019-11-13 22:25:50 +0000

    made build

---
+++ build/precache-manifest.e33ecc3a2ab7d6c8974ee327e1c86fb3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d03416e71a3f2141738fe40414d81fbe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ca19f91a92af6d73f19",
+    "url": "/static/css/main.2199b50f.chunk.css"
+  },
+  {
+    "revision": "b5f0893ef5937e596a3f",
+    "url": "/static/js/2.6fb2ba65.chunk.js"
+  },
+  {
+    "revision": "7ca19f91a92af6d73f19",
+    "url": "/static/js/main.ab7cdfd1.chunk.js"
+  },
+  {
+    "revision": "6aa75ee1c405aa7b66b7",
+    "url": "/static/js/runtime-main.e659958f.js"
+  },
+  {
+    "revision": "97569f09450847de826c517eed71e3e7",
+    "url": "/static/media/default.97569f09.jpg"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6eda5136118a27ce5b3eea599069539e.js"
+  "/precache-manifest.e33ecc3a2ab7d6c8974ee327e1c86fb3.js"
 );
 
 self.addEventListener('message', (event) => {
